7c7b8378d278cc3a0f1c3a152e45dbdd28bae6ec
amfgkzz <48069062+amfgkzz@users.noreply.github.com>
2024-03-01 00:47:41 +0000

    binary search working but not done

--- Algorithms/binarySearch.js
+++ Algorithms/binarySearch.js
@@ -1,12 +1,25 @@
-const sortedArray = [1,2,3,4,5,6,7,8,9,10]
+const sortedArray = [1,2,3,4,5,6,7,8,9,10,11]
 
-function binarySearch(array, size, target){
-  const start = array[0]
-  const middle = array[Math.floor(size / 2)]
-  const end = array[size - 1]
+function binarySearch(array, target){
+  let start = 0
+  let end =  array.length - 1
 
-  console.log(size)
-  console.log(middle)
+  while (start <= end) {
+    console.log("index start: ", start)
+    console.log("index end: ", end)
+    let middle = Math.floor((start + end) / 2)
+    console.log("index middle: ", middle)
+    console.log("middle value: ", array[middle])
+    if (array[middle] < target){
+      start = middle + 1
+    }  
+    else if (array[middle] > target){
+      end = middle - 1
+    }
+    else return middle
+  }
+
+  return -1
 }
 
-binarySearch(sortedArray, sortedArray.length, 9)
+binarySearch(sortedArray, 2)
