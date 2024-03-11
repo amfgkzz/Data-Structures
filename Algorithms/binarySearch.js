5d5460d6335d7ac64c7a30ce8bc946f6b6842fed
amfgkzz <48069062+amfgkzz@users.noreply.github.com>
2024-03-11 23:56:31 +0000

    binary search recursion functional

--- Algorithms/binarySearch.js
+++ Algorithms/binarySearch.js
@@ -1,4 +1,8 @@
-const sortedArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+const sortedArray = []
+
+for(let i = 0; i <= 1000; i++){
+  sortedArray.push(i)
+}
 
 function binarySearch(array, target){
   let start = 0
@@ -20,12 +24,12 @@ function binarySearch(array, target){
 }
 
 binarySearch(sortedArray, 26)
-// binarySearch(sortedArray, 17)
-// binarySearch(sortedArray, 1)
-// binarySearch(sortedArray, 4)
-// binarySearch(sortedArray, 13)
-// binarySearch(sortedArray, 22)
-// binarySearch(sortedArray, 0)
+binarySearch(sortedArray, 17)
+binarySearch(sortedArray, 1)
+binarySearch(sortedArray, 4)
+binarySearch(sortedArray, 13)
+binarySearch(sortedArray, 22)
+binarySearch(sortedArray, 0)
 
 console.log()
 console.log()
@@ -52,45 +56,41 @@ function binarySearchAlternative(array, target){
 }
 
 binarySearchAlternative(sortedArray, 26)
-// binarySearchAlternative(sortedArray, 17)
-// binarySearchAlternative(sortedArray, 1)
-// binarySearchAlternative(sortedArray, 4)
-// binarySearchAlternative(sortedArray, 13)
-// binarySearchAlternative(sortedArray, 22)
-// binarySearchAlternative(sortedArray, 0)
+binarySearchAlternative(sortedArray, 17)
+binarySearchAlternative(sortedArray, 1)
+binarySearchAlternative(sortedArray, 4)
+binarySearchAlternative(sortedArray, 13)
+binarySearchAlternative(sortedArray, 22)
+binarySearchAlternative(sortedArray, 0)
 
 console.log()
 console.log()
 
 function binarySearchRecursion(array, target, start, end){
-  let middle = Math.ceil((start + end) / 2)
+  let middle = Math.floor((start + end) / 2)
 
-  console.log('target: ', target)
-  console.log('start: ', start)
-  console.log('middle: ', middle)
-  console.log('end: ', end)
-  // console.log(target)
-  // console.log(start)
-  // console.log(end)
-  if (array[middle] === target){
-    console.log(`${target} found at index ${middle}`)
-    return middle
-  }
-  
-  if (middle === end && array[middle] !== target){    // recursion method has no stopping point this is needed for that if target is not found
+  if(start > end){
     console.log(`${target} not found`)
     return -1
   }
 
   if(array[middle] < target){
-    return binarySearchRecursion(array, target, middle, end)
+    return binarySearchRecursion(array, target, middle+1, end)
   } else if(array[middle] > target){
-    return binarySearchRecursion(array, target, start, middle)
+    return binarySearchRecursion(array, target, start, middle-1)
+  }
+
+  if(array[middle] == target){
+    console.log(`${target} found at index ${middle}`)
+    return middle
+  } else {
+    console.log(`${target} not found`)
+    return -1
   }
 }
 
 // const sortedArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
-binarySearchRecursion(sortedArray, 1, 0, (sortedArray.length - 1))
+binarySearchRecursion(sortedArray, 1000, 0, (sortedArray.length - 1))
 // binarySearchRecursion(sortedArray, 2, 0, (sortedArray.length - 1))
 // binarySearchRecursion(sortedArray, 25, 0, (sortedArray.length - 1))
